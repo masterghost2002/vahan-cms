452e2021517c2da29171ff92c33a01ab73f5a667
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-05-10 02:30:54 -0400

    Added route to get all the data of the entity

---
+++ controller/entity/entity-data/route.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import getDB from "../../../db";
+export default async function GET(req:Request, res:Response){
+    let entity_name = req.body.entity_name;
+    if(!entity_name || typeof entity_name !== 'string')
+        return res.status(400).json({isError:true, message:'Invalid entity name', data:{}});
+    try {
+        const query = `SELECT * FROM ${entity_name.trim().replace(/\s/g, '_')}`
+        const db = await getDB();
+        const result = await db.query(query);
+        const total = result.rowCount;
+        const rows = result.rows;
+        const fields = result.fields;
+        return res.status(200).json({total, rows, fields});
+    } catch (error:any) {
+        return res.status(500).json({isError:true, message:error.message});
+    }
+}
--- router/entity.router.ts
+++ router/entity.router.ts
@@ -4,10 +4,12 @@ import InsertIntoEntity from '../controller/entity/insert/route';
 import GetAllEntity from '../controller/entity/route';
 import DropEntity from '../controller/entity/drop/route';
 import DeleteRowFromEntity from '../controller/entity/delete-column/route';
+import AllDataFromEntity from '../controller/entity/entity-data/route'
 const entityRouter = Router();
 entityRouter.post('/create', CreateEntity);
 entityRouter.post('/insert', InsertIntoEntity);
 entityRouter.put('/drop', DropEntity);
 entityRouter.delete('/delete-column', DeleteRowFromEntity);
 entityRouter.get('/', GetAllEntity);
+entityRouter.get('/entity-data', AllDataFromEntity);
 export default entityRouter;
