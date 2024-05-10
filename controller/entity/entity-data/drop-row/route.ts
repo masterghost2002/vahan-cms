620d24ffe34972d772cb616ee242e6b99793e452
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-05-10 05:39:31 -0400

    Fixes in drop row controller

--- controller/entity/entity-data/drop-row/route.ts
+++ controller/entity/entity-data/drop-row/route.ts
@@ -3,9 +3,9 @@ import validateQuery from "./validate.query";
 import buildDropRowQuery from "./build-drop-row-query";
 import getDB from "../../../../db";
 export default async function DELETE(req:Request, res:Response){
-    const data = req.body;
+    const data = req.query;
     try {
-        const validate = validateQuery.safeParse(data);
+        const validate = validateQuery.safeParse({entity_name:data.entity_name, condition:{key:data.key, value:data.value}});
         if(!validate.success)
             return res.status(400).json({isError:true, message:'Invalid query data', data:validate.error.errors});
         const queryData = validate.data;
--- controller/entity/entity-data/route.ts
+++ controller/entity/entity-data/route.ts
@@ -5,13 +5,22 @@ export default async function GET(req: Request, res: Response) {
     if (!entity_name || typeof entity_name !== 'string')
         return res.status(400).json({ isError: true, message: 'Invalid entity name', data: {} });
     try {
-        const query = `SELECT * FROM ${entity_name.trim().replace(/\s/g, '_')}`
+        const query = `SELECT * FROM ${entity_name.trim().replace(/\s/g, '_')}`;
+        const primaryKeyQuery = `
+        SELECT a.attname AS column_name
+        FROM pg_index i
+        JOIN pg_attribute a ON a.attrelid = i.indrelid
+                            AND a.attnum = ANY(i.indkey)
+        WHERE i.indrelid = '${entity_name}'::regclass
+          AND i.indisprimary;
+        `
         const db = await getDB();
         const result = await db.query(query);
+        const primaryKey = await db.query(primaryKeyQuery);
         const total = result.rowCount;
         const rows = result.rows;
         const fields = result.fields;
-        return res.status(200).json({ total, rows, fields});
+        return res.status(200).json({primaryKey:primaryKey.rows[0].column_name, total, rows, fields });
     } catch (error: any) {
         return res.status(500).json({ isError: true, message: error.message });
     }
